951a44dd15961509d3fda6a540fc0c8c95e625d2
SwaritTagirisa <85054872+SwaritTagirisa@users.noreply.github.com>
2022-05-15 20:03:09 +1200

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,12 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+  apiKey: "AIzaSyBOuP6C0i_AE3fP-9EhRntC8zk1s2t0_RI",
+  authDomain: "team-voting-p67.firebaseapp.com",
+  projectId: "team-voting-p67",
+  storageBucket: "team-voting-p67.appspot.com",
+  messagingSenderId: "754936788551",
+  appId: "1:754936788551:web:ea5c666ebd434737046362"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
